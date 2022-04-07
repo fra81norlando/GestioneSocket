285afbbb9e28caa01f15ed9fc8108e0d1e4e0df8
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-04-07 10:44:17 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,33 +1,41 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Server {
 	public static void main(String[] args) {
 		try {
-			//server in ascolto 
-			ServerSocket serverSocket=new ServerSocket(2000);
-			System.out.println("Server avviato correttamente");
-			//accetta la richiesta
-			Socket socket=serverSocket.accept();
-			System.out.println("Connessione avvenuta");
-			System.out.println("Socket: " +socket);
-			//invio messaggio di saluto
-			DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
-			outToServer.writeBytes("Ciao");
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String in = inFromClient.toString();
 			
+			ServerSocket serverSocket=new ServerSocket(2000);//apertura socket sulla porta 2000
+			System.out.println("Server avviato correttamente");//stampa di avvio
+			Socket socket=serverSocket.accept();
+			System.out.println("Connessione avvenuta");//stampa di connessione avvenuta
+			System.out.println("Socket: " +socket);
+			DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());//creazione stream di output
+			outToClient.writeBytes("Ciao");//invio messaggio di saluto 
+			
+			DataInputStream inFromClient = new DataInputStream(socket.getInputStream());//creazione stream di input
+			String in = inFromClient.toString();
+			if(in.equals("orario")) {
+				SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd.HH:mm:ss");
+		        String timeStamp = date.format(new Date());// stampa data
+		        outToClient.writeBytes(timeStamp);
+			}
+			
+			serverSocket.close();//chiusura socket
 		} catch(BindException e) {
-			System.err.println("porta occupata");
+			System.err.println("porta occupata");//errore in avvio programma di porta occupata
 			e.printStackTrace();
 		}
 		catch (IOException e) {
-			System.err.println("Errore avvio del server sulla porta 2000");
+			System.err.println("Errore avvio del server sulla porta 2000");//errore in avvio programma con porta 2000 occupata
 			e.printStackTrace();
 		}
 	}
