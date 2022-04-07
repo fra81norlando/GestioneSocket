2de3bc7da9d252b5ea825696f676b72f53d71a4c
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2022-04-07 10:31:57 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -4,17 +4,16 @@ import java.net.Socket;
 public class Client {
 	public static void main(String[] args) {
 		try {
-			String in;
-			Socket socket = new Socket("127.0.0.1",2000);
-			
+			String x;
+			Socket socket = new Socket("127.0.0.1",2000); //istanzio socket(ip+porta)
 			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			in = inFromServer.toString();
-			
+			x = inFromServer.toString();
 			DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
 			outToServer.writeBytes("Sincornizziamo l'ora");
 		} catch (IOException e) {
 			System.err.println("Errore nella connessione al server nella porta 2000");
 			e.printStackTrace();
+			
 		}
 	}
 }
